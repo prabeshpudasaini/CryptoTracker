0d78804a3779d62570bbaf9ac49b0478604bf5f9
Prabesh Pudasaini <pudasainiprabesh56@gmail.com>
2025-06-09 16:15:34 +0000

    Coin List Screen Ui Completed

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,7 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
+import android.icu.text.NumberFormat
 import androidx.annotation.DrawableRes
 import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -19,11 +22,26 @@ data class DisplayableNumber(
     val formatted: String
 )
 
-//fun Coin.toCoinUo(): CoinUi{
-//    return CoinUi(
-//        id = id,
-//        name = name,
-//        symbol = symbol,
-//
-//    )
-//}
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDispalyableNumber(),
+        marketCapUsd = marketCapUsd.toDispalyableNumber(),
+        changePrice24Hr = changePrice24Hr.toDispalyableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDispalyableNumber(): DisplayableNumber {
+    var formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
